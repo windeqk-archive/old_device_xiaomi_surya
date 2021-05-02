fb44b6761561bdb4af458887d0f391ad56c211b2
windeqk <trojan.windows.11@gmail.com>
2021-05-02 20:51:02 +0200

    surya: Adapt to AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_surya.mk
+    $(LOCAL_DIR)/aosp_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_surya-user \
-    arrow_surya-userdebug \
-    arrow_surya-eng
+    aosp_surya-user \
+    aosp_surya-userdebug \
+    aosp_surya-eng
