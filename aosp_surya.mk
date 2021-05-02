170452034255a3eccfc29f5cea602809e9fae8de
windeqk <trojan.windows.11@gmail.com>
2021-05-02 20:51:02 +0200

    surya: Adapt to PixelOS

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -9,19 +9,23 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X3 NFC
+PRODUCT_MODEL := POCO X3
 PRODUCT_MANUFACTURER := Xiaomi
-DEVICE_MAINTAINER := Ganesh Varma
 
-BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.3.0.QJGMIXM:user/release-keys
-TARGET_INCLUDE_PIXEL_CHARGER := true
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
