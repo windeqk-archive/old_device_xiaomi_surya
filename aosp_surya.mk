b9e555d5fda55e9babc556f61a8c3f5f5ef4b4f0
windeqk <trojan.windows.11@gmail.com>
2021-05-06 22:07:04 +0200

    surya: Small cleanup

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -4,20 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
@@ -30,5 +34,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
