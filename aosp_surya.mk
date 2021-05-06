10421c6c831d77c83b9d53fd22c4cafe79536ebc
windeqk <trojan.windows.11@gmail.com>
2021-05-06 09:51:27 +0200

    surya: Add face unlock

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -4,18 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common PixelishExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_USES_AOSP_RECOVERY := true
+
+# Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_SCREEN_HEIGHT := 2400
@@ -32,3 +35,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Face unlock
+TARGET_FACE_UNLOCK := true
