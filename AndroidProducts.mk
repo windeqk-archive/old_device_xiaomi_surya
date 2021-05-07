a11c42988fc08f2abf6bdf78422e8106c526e4f1
windeqk <trojan.windows.11@gmail.com>
2021-05-07 22:07:54 +0200

    surya: Adapt to NezukoOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_surya.mk
+    $(LOCAL_DIR)/nezuko_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_surya-user \
-    aosp_surya-userdebug \
-    aosp_surya-eng
+    nezuko_surya-user \
+    nezuko_surya-userdebug \
+    nezuko_surya-eng
--- nezuko_surya.mk
+++ nezuko_surya.mk
@@ -11,8 +11,8 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common NezukoOS stuff.
+$(call inherit-product, vendor/nezuko/config/common_full_phone.mk)
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
@@ -26,7 +26,7 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_surya
+PRODUCT_NAME := nezuko_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
@@ -34,3 +34,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
+# Nezuko OS Properties
+CUSTOM_BUILD_TYPE := UNOFFICIAL
+NEZUKO_MAINTAINER := Windeqk
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.nezuko.cpu=SD732G
